27e5f33c7da6bf8d8fb68cb5c8291fe7106b205f
Dotista <dmessina@outlook.be>
2025-08-20 23:04:47 +0200

    Fix auto font size

--- wargame_site/static/js/textFit.js
+++ wargame_site/static/js/textFit.js
@@ -3,13 +3,30 @@ function adjustFontSizeToFit() {
     if (!highlightBox.offsetWidth || !highlightBox.offsetHeight) return;
     const container = highlightBox;
     let fontSize = currentFontSize;
-    htmlPreview.style.fontSize = fontSize + 'px';
     htmlPreview.style.width = '100%';
     htmlPreview.style.height = 'auto';
-    while (htmlPreview.scrollHeight > container.clientHeight && fontSize > 6) {
+    htmlPreview.style.fontSize = fontSize + 'px';
+
+    // First, try growing the text until it no longer fits
+    while (
+        htmlPreview.scrollHeight <= container.clientHeight &&
+        htmlPreview.scrollWidth <= container.clientWidth &&
+        fontSize < 300
+    ) {
+        fontSize++;
+        htmlPreview.style.fontSize = fontSize + 'px';
+    }
+
+    // If we overflowed, shrink until it fits
+    while (
+        (htmlPreview.scrollHeight > container.clientHeight ||
+            htmlPreview.scrollWidth > container.clientWidth) &&
+        fontSize > 6
+    ) {
         fontSize--;
         htmlPreview.style.fontSize = fontSize + 'px';
     }
+
     currentFontSize = fontSize;
     if (typeof updateFontDisplay === "function") updateFontDisplay();
 }
